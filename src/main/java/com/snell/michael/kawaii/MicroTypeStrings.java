2f2093912905092151c755aa5aa952a79f6aa1af
Michael Snell <michael@snell.com>
2015-03-31 11:04:55 +0100

    Improve cloning

--- src/main/java/com/snell/michael/kawaii/MicroTypeStrings.java
+++ src/main/java/com/snell/michael/kawaii/MicroTypeStrings.java
@@ -9,19 +9,14 @@ public class MicroTypeStrings {
     private MicroTypeStrings() {}
 
     public static <T extends MicroType<String>> T toUpperCase(T microTypeString) {
-        if (microTypeString == null) {
-            return null;
-        } else {
-            return newMicroType(microTypeString, microTypeString.value().toUpperCase());
-        }
+        return microTypeString == null ? null : newMicroType(microTypeString, microTypeString.value().toUpperCase());
     }
 
     public static <T extends MicroType<String>> T toLowerCase(T microTypeString) {
-        if (microTypeString == null) {
-            return null;
-        } else {
-            //noinspection unchecked
-            return newMicroType(microTypeString, microTypeString.value().toLowerCase());
-        }
+        return microTypeString == null ? null : newMicroType(microTypeString, microTypeString.value().toLowerCase());
+    }
+
+    public static <T extends MicroType<String>> T trim(T microTypeString) {
+        return microTypeString == null ? null : newMicroType(microTypeString, microTypeString.value().trim());
     }
 }
