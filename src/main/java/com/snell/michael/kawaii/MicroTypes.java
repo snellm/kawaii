635adaf195c38b238ed4b1c626e3a2c07dcdd11c
Michael Snell <michael@snell.com>
2015-04-02 14:43:40 +0100

    Add reflection caching to MicroTypes

--- src/main/java/com/snell/michael/kawaii/MicroTypes.java
+++ src/main/java/com/snell/michael/kawaii/MicroTypes.java
@@ -1,14 +1,19 @@
 package com.snell.michael.kawaii;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Utility methods for MicroTypes
  */
-// TODO Add caching for reflective newMicroType and constructor lookup?
 public class MicroTypes {
+    private static final Map<Class<? extends MicroType>, Constructor<? extends MicroType>> CONSTRUCTOR_CACHE = new ConcurrentHashMap<>();
+    private static final Map<Class<? extends MicroType>, Optional<Method>> NEWMICROTYPE_METHOD_CACHE = new ConcurrentHashMap<>();
+
     private MicroTypes() {}
 
     /**
@@ -22,19 +27,20 @@ public class MicroTypes {
      * @return A newly created instance of the microtype
      */
     public static <M extends MicroType<T>, T> M newMicroType(Class<M> clazz, T value) {
-        try {
-            Method method = clazz.getMethod("newMicroType", value.getClass());
+        Optional<Method> newMicroTypeMethod = getNewMicroTypeMethod(clazz, value);
+
+        if (newMicroTypeMethod.isPresent()) {
             try {
                 //noinspection unchecked
-                return (M) method.invoke(null, value);
+                return (M) newMicroTypeMethod.get().invoke(null, value);
             } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException("getInstance method exists but failed on [" + clazz + "]", e);
+                throw new RuntimeException("getInstance method exists but failed on [" + clazz + "] with value [" + value + "]", e);
             }
-        } catch (NoSuchMethodException e) {
+        } else {
             try {
-                return clazz.getDeclaredConstructor(value.getClass()).newInstance(value);
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException f) {
-                throw new RuntimeException("Unable to create new instance of [" + clazz + "] with value [" + value + "]", f);
+                return getConstructor(clazz, value).newInstance(value);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException f) {
+                throw new RuntimeException("Unable to create new instance of [" + clazz + "] with value [" + value + "] via constructor", f);
             }
         }
     }
@@ -85,6 +91,33 @@ public class MicroTypes {
             return Optional.empty();
         } else {
             return Optional.of(newMicroType(clazz, value));
+        }
+    }
+
+    private static <M extends MicroType<T>, T> Optional<Method> getNewMicroTypeMethod(Class<M> clazz, T value) {
+        Optional<Method> method = NEWMICROTYPE_METHOD_CACHE.get(clazz);
+        if (method == null) {
+            try {
+                method = Optional.of(clazz.getMethod("newMicroType", value.getClass()));
+            } catch (NoSuchMethodException e) {
+                method = Optional.empty();
+            }
+            NEWMICROTYPE_METHOD_CACHE.put(clazz, method);
+        }
+        return method;
+    }
+
+    private static <M extends MicroType<T>, T> Constructor<M> getConstructor(Class<M> clazz, T value) {
+        try {
+            //noinspection unchecked
+            Constructor<M> constructor = (Constructor<M>) CONSTRUCTOR_CACHE.get(clazz);
+            if (constructor == null) {
+                constructor = clazz.getDeclaredConstructor(value.getClass());
+                CONSTRUCTOR_CACHE.put(clazz, constructor);
+            }
+            return constructor;
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("No suitable constructor for [" + clazz + "] with value [" + value + "]", e);
         }
     }
 }
--- src/main/java/com/snell/michael/kawaii/primitive/BooleanMicroType.java
+++ src/main/java/com/snell/michael/kawaii/primitive/BooleanMicroType.java
@@ -1,5 +1,6 @@
 package com.snell.michael.kawaii.primitive;
 
+// TODO Should this exist, or should enums be used for boolean microtypes?
 public abstract class BooleanMicroType extends PrimitiveMicroType<Boolean> {
     public BooleanMicroType(Boolean value) {
         super(value);
